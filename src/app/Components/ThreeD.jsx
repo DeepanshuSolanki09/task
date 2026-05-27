87cb7441d08f0f18ceb2345c54a72434dbc8d2c2
Deepanshu solanki <solankideepanshu2006@gmail.com>
2026-05-27 19:54:03 +0530

    Update model scale in ThreeD component

--- src/app/Components/ThreeD.jsx
+++ src/app/Components/ThreeD.jsx
@@ -7,7 +7,7 @@ import { Loader } from '@react-three/drei';
 
 const Model = () => {
   const { scene } = useGLTF("/Untitled2.glb");
-  return <primitive object={scene} scale={[5, 5, 9]} position={[0, -4, 0]} />;
+  return <primitive object={scene} scale={[5, 5, 12]} position={[0, -4, 0]} />;
 };
 
 const ThreeD = () => {
