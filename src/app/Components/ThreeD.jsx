5b892a915a8aecd222077da22ddbbe1cdc826809
Deepanshu solanki <solankideepanshu2006@gmail.com>
2026-05-27 19:56:43 +0530

    Adjust model scale and camera position in ThreeD component

--- src/app/Components/ThreeD.jsx
+++ src/app/Components/ThreeD.jsx
@@ -7,13 +7,13 @@ import { Loader } from '@react-three/drei';
 
 const Model = () => {
   const { scene } = useGLTF("/Untitled2.glb");
-  return <primitive object={scene} scale={[5, 5, 12]} position={[0, -4, 0]} />;
+  return <primitive object={scene} scale={[5, 5, 5]} position={[0, -4, 0]} />;
 };
 
 const ThreeD = () => {
   return (
     <div className="h-[450px] w-full">
-      <Canvas camera={{ position: [0, 7, 4], fov: 45 }}>
+      <Canvas camera={{ position: [0, 7, 9], fov: 45 }}>
         <Suspense fallback={null}>
           <Model />
         </Suspense>
